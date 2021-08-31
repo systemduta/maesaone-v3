5c3e3cc99ba5e92b1f2c0a470517c76c7b4727ac
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2021-08-31 12:49:06 +0700

    update config meetwebsite package excel:

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Kawankoding\Fcm\FcmServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -233,6 +234,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         'Fcm' => Kawankoding\Fcm\FcmFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
